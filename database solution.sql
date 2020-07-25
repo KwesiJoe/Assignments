7843f8545461a8cca3c9a8ed69fa26bac92e640d
KwesiJoe <67046252+KwesiJoe@users.noreply.github.com>
2020-07-25 11:34:59 +0000

    Update database solution.sql

--- database solution.sql
+++ database solution.sql
@@ -14,13 +14,12 @@ where send_amount_currency = 'CFA';
 
 --4 Number of agent transactions grouped by months in 2018. month_created is the month extracted from the when_created field 
 SELECT EXTRACT(month FROM when_created) as month_created, COUNT(*) FROM "PostgreSQL".agent_transactions
-WHERE EXTRACT(year from when_created) = '2018'
-GROUP BY EXTRACT(month from when_created)
+WHERE EXTRACT(year FROM when_created) = '2018'
+GROUP BY EXTRACT(month FROM when_created)
 
 
---5 To get the number of net depositors and withdrawers, I first get tag the sum of the amount of each agent as a 'net withdrawer' if the sum is positive 
+--5 To get the number of net depositors and withdrawers, I first tag the sum of the amount of each agent as a 'net withdrawer' if the sum is positive 
 --or a 'net depositer' if the sum is negative. Then count the result of the query for each tag. 
-
 SELECT status, COUNT(status) FROM (
 SELECT SUM(amount),
 CASE
@@ -36,39 +35,33 @@ AS net
 GROUP BY status
 ;
 
---6
-
-
-
+--6 “atx volume city summary” table: volume of agent transactions created
+--in the last week, grouped by city.
 SELECT agents.city, sum(agent_transactions.amount) as volume 
-
 INTO "atx volume city summary"
-
 FROM "PostgreSQL".agent_transactions
 JOIN "PostgreSQL".agents
-
 ON agents.agent_id = agent_transactions.agent_id
-
 WHERE agent_transactions.when_created > NOW() - INTERVAL '7 days'
 GROUP BY city
 ;
 
 
-
---7
+--7 grouping city by country in the “atx volume city summary” table
 
 SELECT  agents.country, agents.city, "atx volume city summary".volume 
 FROM "PostgreSQL".agent_transactions
 JOIN "atx volume city summary"
-
 ON agents.city = "atx volume city summary".city
 WHERE agent_transactions.when_created > NOW() - INTERVAL '7 days'
 GROUP BY city, country
 ;
 
+--8 
+
+
 
 --10
-
 SELECT wallets.wallet_id, sum(transfers.send_amount_scalar) 
 FROM "PostgreSQL".wallets
 JOIN "PostgreSQL".transfers
